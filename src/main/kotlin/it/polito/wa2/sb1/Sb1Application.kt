23ad72868833d5b6198474a9af66477a2ebfcf82
Sefa Kurtoğlu <bmkurtoglusefa@gmail.com>
2025-03-14 17:14:45 +0100

    Initial commit

---
+++ src/main/kotlin/it/polito/wa2/sb1/Sb1Application.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.sb1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sb1Application
+
+fun main(args: Array<String>) {
+	runApplication<Sb1Application>(*args)
+}
